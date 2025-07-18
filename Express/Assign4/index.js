8e37406a9c5dded1d1daf491746feb47290218c2
Uday246r <udaychauhan246r@gamil.com>
2025-07-18 15:06:24 +0530

    Assign4 added

---
+++ Express/Assign4/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const route = express.Router();
+const server = express();
+
+route.get('/greet',(req,res)=>{
+    const greetings = {
+        en : "Hello",
+        fr: "Bonjour",
+        hi: "Namaste"
+    };
+
+    const lang = req.query.lang;
+
+    const message = greetings[lang] || "Hello";
+
+    res.send(message);
+})
+
+server.use('/',route);
+
+server.listen(3000,()=>{
+    console.log("server started...");
+});
