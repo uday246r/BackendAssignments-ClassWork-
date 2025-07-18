dc86d53a7ee25b79cba1508b862d0f145cc521be
Uday246r <udaychauhan246r@gamil.com>
2025-07-18 15:42:11 +0530

    assignment6 and express module completed

---
+++ Express/Assign6/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const users = [
+  { id: '101', name: 'Alice', age: 25 },
+  { id: '102', name: 'Bob', age: 30 },
+  { id: '103', name: 'Charlie', age: 22 }
+];
+
+app.get("/user/:id",(req,res)=>{
+    const { id } = req.params;
+    const person = users.find(users=>users.id==id);
+    res.send(`${person.name} has id ${person.id} is ${person.age} years old.`)
+})
+
+app.listen(3000,()=>{
+    console.log("server started...")
+})
